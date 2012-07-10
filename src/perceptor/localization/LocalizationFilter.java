5b68f5a801421fcf65b14df2b05781a7710b0c8e
Giorgos <giorgosmethe@gmail.com>
2012-07-10 15:19:01 +0300

    AST_3D Version 2.5.4 Included localization filter which had been ignored in previous commit

---
+++ src/perceptor/localization/LocalizationFilter.java
@@ -0,0 +1,80 @@
+package perceptor.localization;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class LocalizationFilter {
+
+	public static CompleteCoordinate MyFilteredPosition;
+
+	public static Queue<CompleteCoordinate> qe = new LinkedList<CompleteCoordinate>();
+
+	public static void filter(CompleteCoordinate LocalizationSample) {
+
+		if (!Double.isNaN(LocalizationSample.getX())
+				&& !Double.isNaN(LocalizationSample.getY())
+				&& !Double.isNaN(LocalizationSample.getTheta())) {
+
+			if (qe.size() == 0) {
+
+				qe.add(LocalizationSample);
+				MyFilteredPosition = VectorAVG();
+
+			} else if (qe.size() < 10) {
+
+				MyFilteredPosition = VectorAVG();
+				if (TriangleLocalization.FindDistanceAmong2Coordinates(
+						new Coordinate(MyFilteredPosition.getX(),
+								MyFilteredPosition.getY()),
+						new Coordinate(LocalizationSample.getX(),
+								LocalizationSample.getY())) > 2) {
+
+					qe.remove();
+
+				} else {
+
+					qe.add(LocalizationSample);
+
+				}
+
+			} else {
+
+				MyFilteredPosition = VectorAVG();
+				qe.remove();
+
+			}
+
+		}
+
+	}
+
+	public static CompleteCoordinate VectorAVG() {
+
+		double x = 0, y = 0, theta = 0, sinSum = 0, cosSum = 0;
+
+		Iterator it = qe.iterator();
+
+		while (it.hasNext()) {
+
+			CompleteCoordinate a = (CompleteCoordinate) it.next();
+
+			x += a.getX();
+			y += a.getY();
+
+			cosSum += Math.cos(Math.toRadians(a.getTheta()));
+
+			sinSum += Math.sin(Math.toRadians(a.getTheta()));
+
+		}
+
+		theta = Math.toDegrees(Math.atan2(sinSum, cosSum));
+
+		CompleteCoordinate MyAvgPosition = new CompleteCoordinate(
+				(x / qe.size()), (y / qe.size()), theta);
+
+		return MyAvgPosition;
+
+	}
+
+}
